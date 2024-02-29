9fe6c9cc9a65dedf224455aba301c39eaf88bae6
Jacek Michalski <jacmilski@gmail.com>
2024-02-29 14:48:55 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `my-best-party-page`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components"]
+};
