1fe88e09063a2df580a78656390257d8248af29a
Jacek Michalski <jacmilski@gmail.com>
2024-03-07 01:32:14 +0100

    Fixed configuration in gatsby-browser file

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,32 +5,3 @@ import GlobalLayout from './src/components/templates/GlobalLayout/GlobalLayout';
 export const wrapPageElement = ({ element, props }) => {
     return <GlobalLayout {...props}>{ element }</GlobalLayout>
 }
-
-export const onRenderBody = ({ setHeadComponents }) => {
-    setHeadComponents([
-        <link
-            rel='preload'
-            href='/fonts/Roboto-Light.woff2'
-            as='font'
-            type='font/woff2'
-            crossOrigin='anonymus'
-            key='interFont'
-        />,
-        <link
-            rel='preload'
-            href='/fonts/Roboto-Regular.woff2'
-            as='font'
-            type='font/woff2'
-            crossOrigin='anonymus'
-            key='interFont'
-        />,
-        <link
-            rel='preload'
-            href='/fonts/Roboto-Bold.woff2'
-            as='font'
-            type='font/woff2'
-            crossOrigin='anonymus'
-            key='interFont'
-        />
-    ])
-}
