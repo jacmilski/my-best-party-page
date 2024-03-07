b5ba7887d52fab2cf9b8b203eb8adbcc38ef58eb
Jacek Michalski <jacmilski@gmail.com>
2024-03-07 01:18:47 +0100

    Created GlobalLayout and its configuration in gatsby-ssr and gatsby-browser files

---
+++ gatsby-browser.js
@@ -0,0 +1,7 @@
+// @ts-nocheck
+import React from 'react';
+import GlobalLayout from './src/components/templates/GlobalLayout/GlobalLayout';
+
+export const wrapPageElement = ({ element, props }) => {
+    return <GlobalLayout {...props}>{ element }</GlobalLayout>
+}
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,4 +1,10 @@
+// @ts-nocheck
 const React = require("react");
+const GlobalLayout = require('./src/components/templates/GlobalLayout/GlobalLayout').default;
+
+exports.wrapPageElement = ({ element, props }) => {
+    return <GlobalLayout {...props}>{ element }</GlobalLayout>
+}
 
 exports.onRenderBody = ({ setHeadComponents }) => {
     setHeadComponents([
